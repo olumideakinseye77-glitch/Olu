11c848980a882c01ff5884746006c3bbe0ae1db6
olumideakinseye77-glitch <olumideakinseye77@gmail.com>
2026-09-25 07:23:22 +0100

    Prepare Project 2 Terraform staging: infra/versions.tf

---
+++ infra/versions.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_version = ">= 1.6.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+  }
+
+  # Before 'terraform apply', configure durable state storage as described
+  # in infra/README.md and change this to: backend "azurerm" {}
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = var.subscription_id
+}
